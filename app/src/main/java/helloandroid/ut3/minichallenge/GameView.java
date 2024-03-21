05622fbfc01c359bf7d6cb7402b84ae296971c1d
Olympe Heurtebise <olympe.heurtebise@orange.fr>
2024-03-21 14:52:54 +0100

    Pré set-up

--- app/src/main/java/helloandroid/ut3/minichallenge/GameView.java
+++ app/src/main/java/helloandroid/ut3/minichallenge/GameView.java
@@ -11,17 +11,32 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
     public GameView(Context context) {
         super(context);
+        thread = new GameThread(getHolder(), this);
+        setFocusable(true);
         getHolder().addCallback(this);
     }
 
+    public void update() {    }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
     }
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        thread.setRunning(true);
+        thread.start();
     }
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
     }
 }
